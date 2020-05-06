050f444985dd5be50aff5370c88b8353a2e32297
GerardFeixes <59616754+GerardFeixes@users.noreply.github.com>
2020-05-07 00:13:18 +0200

    testing planet gravity

---
+++ Assets/Scripts/Player/SphereWorld/GravityBody.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    GravityAttractor planet;
+    void Awake()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        planet.Attract(transform);
+    }
+}
