e7e538e50e978a5d582bb51de96cb184ab7a727c
AlvaroOlivetGimeno <alvarolivet@gmail.com>
2020-05-11 15:54:42 +0200

    Revert "nidea"
    This reverts commit aa629fe8b92f5b1940197d8c0342bf6f0fcf4ac0, reversing
    changes made to 43be5e661725ae423bc2dbf25d58c23342423707.

--- Assets/Scripts/Player/HippiCharacterController.cs
+++ Assets/Scripts/Player/HippiCharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (BLACKBOARD_ThirdPersonCharacter))]
-//[RequireComponent(typeof (CharacterController))]
+[RequireComponent(typeof (CharacterController))]
 public class HippiCharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
