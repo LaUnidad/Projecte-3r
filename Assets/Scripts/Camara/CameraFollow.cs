436642a1fed3840aa16b035432a968fef6821c5b
GerardFeixes <59616754+GerardFeixes@users.noreply.github.com>
2020-05-15 22:08:02 +0200

    poco

--- Assets/Scripts/Camara/CameraFollow.cs
+++ Assets/Scripts/Camara/CameraFollow.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject player;
+
+    public Transform playerTransform;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1f)]
+    public float smoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+
     void Start()
     {
-        
+        cameraOffset = transform.position - playerTransform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        this.transform.position = player.transform.position;
+        Vector3 newPos = playerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        if (LookAtPlayer) transform.LookAt(playerTransform);
     }
 }
