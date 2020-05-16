e4619164f70b036de0d8d90292a8486ba2112d48
GerardFeixes <59616754+GerardFeixes@users.noreply.github.com>
2020-05-16 22:32:10 +0200

    camera finished

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform playerTransform;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1f)]
+    public float smoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+    public bool rotateAroundPlayer = false;
+
+    public float rotationSpeed = 5f;
+
+    void Start()
+    {
+        cameraOffset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (rotateAroundPlayer)
+        {
+            Quaternion camTurnAngleX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+             Quaternion camTurnAngleY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotationSpeed, Vector3.right);
+
+            //Quaternion camTurnAgnle = Quaternion.Angle(camTurnAngleX, camTurnAngleY);
+
+            cameraOffset = camTurnAngleX * cameraOffset;
+        }
+
+        //cameraOffset.magnitude += Input.GetAxis("Mouse ScrollWheel");
+        //cameraOffset = Mathf.Clamp(ve)
+
+
+        Vector3 newPos = playerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        
+
+        if (Input.GetMouseButton(1))
+        {
+            cameraOffset = transform.position - playerTransform.position;
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+            //if(mouseScroll <=)
+            transform.position = transform.position += (cameraOffset * mouseScroll);
+
+            
+            Debug.LogWarning("MouseScroll: " + mouseScroll);
+        }
+
+    
+        if (LookAtPlayer || rotateAroundPlayer) transform.LookAt(playerTransform);
+
+         //Vector3 newRotation = Vector3.RotateTowards(transform.forward, -cameraOffset, rotationSpeed * Time.deltaTime, 0f);
+
+        //transform.rotation = Quaternion.LookRotation(newRotation);
+       
+        Debug.Log("CameraOffset : " + cameraOffset);
+    }
+    /*
+    public void CameraZoom(Vector3 cameraOffset, float radius)
+    {
+        cameraOffset.magnitude
+    }
+    */
+}
---
+++ Assets/Scripts/Camera/CameraOrbit.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Transform parentTransform;
+
+    private Vector3 localRotation;
+    private float cameraDistance = 10f;
+
+    public float mouseSensitivity = 4f;
+    public float scrollSensitivity = 2f;
+    public float orbitDampening = 10f;
+    public float scrollDampening = 8f;
+
+    public bool cameraDisabled = false;
+
+    // Camera Collision
+
+    public LayerMask collisionMask;
+    public bool collisionRayDebug = false;
+
+    public float collisionOffset = .5f;
+    public float adjustedDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = this.transform;
+        parentTransform = this.transform.parent;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //if (Input.GetKeyDown(KeyCode.LeftShift)) cameraDisabled = !cameraDisabled;
+
+        if(!cameraDisabled)
+        {
+            if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                localRotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+                localRotation.y -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+                //Clamp rotation angle
+                localRotation.y = Mathf.Clamp(localRotation.y, -30f, 70f);
+            }
+
+            //Zoom with Mouse scroll
+            if(Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
+
+                //Zoom faster the further away the camera is
+                scrollAmount *= cameraDistance * 0.3f;
+
+                cameraDistance += scrollAmount * -1f;
+
+                cameraDistance = Mathf.Clamp(cameraDistance, 6f, 16f);
+
+            }
+        }
+
+        Quaternion qt = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+        parentTransform.rotation = Quaternion.Lerp(parentTransform.rotation, qt, Time.deltaTime * orbitDampening);
+
+        if(cameraTransform.localPosition.z != adjustedDistance * -1f)
+        {
+            //cameraTransform.localPosition = new Vector3(0f, 0f, Mathf.Lerp(cameraTransform.localPosition.z, cameraDistance * -1, Time.deltaTime * scrollDampening));
+            cameraTransform.localPosition = new Vector3(0f, 0f, Mathf.Lerp(cameraTransform.localPosition.z, adjustedDistance * -1, Time.deltaTime * scrollDampening));
+        }
+
+        CameraCollision();
+    }
+
+    void CameraCollision()
+    {
+        float camDistance = cameraDistance + collisionOffset;
+
+        Ray camRay = new Ray(parentTransform.position, -cameraTransform.forward);
+        RaycastHit camRayHit;
+
+        if(Physics.Raycast(camRay, out camRayHit, camDistance, collisionMask))
+        {
+            adjustedDistance = Vector3.Distance(camRay.origin, camRayHit.point) - collisionOffset;
+        }
+        else
+        {
+            adjustedDistance = cameraDistance;
+        }
+
+        if (collisionRayDebug)
+            Debug.DrawLine(camRay.origin, camRay.origin + camRay.direction * cameraDistance, Color.cyan);
+
+    }
+}
