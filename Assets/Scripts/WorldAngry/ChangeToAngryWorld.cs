3ada842729191a3f37ce4a598c1d674a5c37387d
AlvaroOlivetGimeno <alvarolivet@gmail.com>
2020-05-20 22:24:21 +0200

    CARAVANA Y CAMBIO DE MUNDO

---
+++ Assets/Scripts/WorldAngry/ChangeToAngryWorld.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToAngryWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] TerrainColor;
+    GameObject[] MagneticRocks;
+    void Start()
+    {
+        TerrainColor = GameObject.FindGameObjectsWithTag("Terrain");
+        MagneticRocks = GameObject.FindGameObjectsWithTag("MagneticRock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChangeWorld()
+    {
+        foreach(GameObject obj in TerrainColor)
+        {
+            obj.GetComponent<TerrainMaterial>().Die = true;
+        }
+        foreach(GameObject obj in MagneticRocks)
+        {
+
+            obj.gameObject.tag = "AspirableObject";
+        }
+    }
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Player")
+        {
+            ChangeWorld();
+        }    
+    }
+}
