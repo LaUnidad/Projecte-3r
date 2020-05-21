5aa8fc8bbb6d374bd7e8154c8a0592c108f46099
mComesm <59616816+mComesm@users.noreply.github.com>
2020-05-21 18:16:03 +0200

    Activate Objects Distance

---
+++ Assets/Scripts/Optimitzation/DisableIfFar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableIfFar : MonoBehaviour
+{
+    private GameObject itemActivatorObject;
+    private ObjectActivator activationScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemActivatorObject = GameObject.Find("ItemActivatorObject");
+        activationScript = itemActivatorObject.GetComponent<ObjectActivator>();
+
+        StartCoroutine("AddToList");
+    }
+
+    IEnumerator AddToList()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        activationScript.m_activatorItems.Add(new ActivatorItem { item = this.gameObject, itemPos = transform.position });
+    }
+
+    
+}
