2169a5773691dcb1af515644b01fe7dcd63bc47e
GerardFeixes <59616754+GerardFeixes@users.noreply.github.com>
2020-05-30 02:54:59 +0200

    test postpro 80

---
+++ Assets/Scripts/DestructableWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableWall : MonoBehaviour
+{
+
+    public GameObject destroyedObject;
+
+
+    void OnMouseDown()
+    {
+        Instantiate(destroyedObject, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Instantiate(destroyedObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
