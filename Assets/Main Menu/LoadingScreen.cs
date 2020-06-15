2b46fd11f43ec308b53092ac078264bf301b8dac
mComesm <59616816+mComesm@users.noreply.github.com>
2020-06-15 16:04:52 +0200

    Menu & Loading Right

---
+++ Assets/Main Menu/LoadingScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Image _progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //create an async operation
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        //Fill the bar = async operation progress
+        while (gameLevel.progress < 1)
+        {
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
--- Assets/Main Menu/MainMenu.cs
+++ Assets/Main Menu/MainMenu.cs
@@ -7,12 +7,44 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public float time = 10f;
+    public float time = 6f;
     public Animator anim;
     public Camera cam;
 
-    public GameObject OptionsFirstButton, MenuFirstSelected;
+    public GameObject OptionsFirstButton, MenuFirstSelected, ToogleFullScreen;
     public GameObject m_Options;
+
+    public GameObject volumeImage, fullScreenImage;
+
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     private void Update()
     {
@@ -21,6 +53,18 @@ public class MainMenu : MonoBehaviour
             m_Options.SetActive(false);
             Back();
         }
+
+        if (EventSystem.current.currentSelectedGameObject == OptionsFirstButton)
+        {
+            volumeImage.GetComponent<Animation>().Play("SoundAnim");
+        }
+        else volumeImage.GetComponent<Animation>().Stop();
+
+        if (EventSystem.current.currentSelectedGameObject == ToogleFullScreen)
+        {
+            fullScreenImage.GetComponent<Animation>().Play("FullScreenAnim");
+        }
+        else fullScreenImage.GetComponent<Animation>().Stop();
     }
 
     public void PlayGame()
@@ -48,6 +92,17 @@ public class MainMenu : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(MenuFirstSelected);
     }
 
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
@@ -57,7 +112,7 @@ public class MainMenu : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
 
-        SceneManager.LoadScene("ProvesTerrain");
+        SceneManager.LoadScene(1);
 
     }
 
