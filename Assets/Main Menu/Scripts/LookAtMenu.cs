dd92556c33360ebd2b0326ab13284f5fc61c1637
mComesm <59616816+mComesm@users.noreply.github.com>
2020-06-17 02:09:09 +0200

    New Menu

---
+++ Assets/Main Menu/Scripts/LookAtMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMenu : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+
+    
+}
