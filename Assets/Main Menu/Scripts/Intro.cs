bd7ec78f1266b2447d782599e2211d36271b99ed
mComesm <59616816+mComesm@users.noreply.github.com>
2020-06-18 01:46:25 +0200

    Intro

---
+++ Assets/Main Menu/Scripts/Intro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+
+    public GameObject g1, g2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(IntroCo());
+    }
+
+
+    IEnumerator IntroCo()
+    {
+        yield return new WaitForSeconds(15f);
+        Destroy(g1);
+        Destroy(g2);
+    }
+}
